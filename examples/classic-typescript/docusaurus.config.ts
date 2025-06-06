265515c799a15ab4ba773f585197ef66030288c7
Danesh <danesh.permeh@gmail.com>
2025-06-06 03:39:09 +0330

    Update docusaurus.config.ts

--- examples/classic-typescript/docusaurus.config.ts
+++ examples/classic-typescript/docusaurus.config.ts
@@ -10,10 +10,10 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://docs-ezping.netlify.app/',
+  url: 'https://docs.ezping.ir/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/build/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
